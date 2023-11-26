a43fb2e1e2198bb21b571cd01082b49600e0d12f
Liam <liam.jacobsalangsang@gmail.com>
2023-11-27 06:45:30 +0700

    commiting files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function checkInput() {
+    var inputField = document.getElementById("emailsubmission");
+    var inputValue = inputField.value.trim();
+  
+    if (inputValue === '') {
+      inputField.focus();
+      return false; // Prevent form submission if the input is empty
+    }
+    return true; // Allow form submission if the input is not empty
+  }
+  
+
+  
